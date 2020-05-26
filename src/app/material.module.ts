54c57d7225dfd63641c60e9e49657634b4adc207
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2020-05-26 23:13:26 +0200

    34. Adding a Checkbox also added missing name for birthdate input

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,24 +5,26 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   imports: [
     MatButtonModule,
+    MatCheckboxModule,
     MatDatepickerModule,
-    MatIconModule,
     MatFormFieldModule,
+    MatIconModule,
     MatInputModule,
     MatNativeDateModule,
   ],
   exports: [
     MatButtonModule,
+    MatCheckboxModule,
     MatDatepickerModule,
-    MatIconModule,
     MatFormFieldModule,
+    MatIconModule,
     MatInputModule,
     MatNativeDateModule,
-
   ]
 })
 export class MaterialModule {
