78824eca81e6e6c7cd007330705fcc8199b89e35
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2020-05-26 22:57:22 +0200

    32. Adding a Datepicker and needed imports to material.module.ts

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,19 +3,25 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   imports: [
     MatButtonModule,
+    MatDatepickerModule,
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
+    MatNativeDateModule,
   ],
   exports: [
     MatButtonModule,
+    MatDatepickerModule,
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
+    MatNativeDateModule,
 
   ]
 })
