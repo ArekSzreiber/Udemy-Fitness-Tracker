391150ccc0d6662042fd2112060b288396bc194b
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2020-05-29 01:42:26 +0200

    48. Adding a Dropdown Menu

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -11,6 +11,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   exports: [
@@ -23,6 +24,7 @@ import { MatCardModule } from '@angular/material/card';
     MatInputModule,
     MatListModule,
     MatNativeDateModule,
+    MatSelectModule,
     MatSidenavModule,
     MatTabsModule,
     MatToolbarModule,
