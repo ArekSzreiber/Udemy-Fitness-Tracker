9c5b4ee81a67e00a84266a599006de5930483f0e
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2020-05-29 01:16:53 +0200

    47. Adding some "Cards"

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -10,10 +10,12 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   exports: [
     MatButtonModule,
+    MatCardModule,
     MatCheckboxModule,
     MatDatepickerModule,
     MatFormFieldModule,
