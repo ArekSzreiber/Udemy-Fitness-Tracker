575cbf5a6402797554aa67e1ee9f08b17b4e262d
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2021-01-20 21:45:04 +0100

    107. Creating a SharedModule

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-import { MaterialModule } from '../material.module';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,11 +12,8 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
   ],
   imports: [
     AngularFireAuthModule,
-    CommonModule,
-    FormsModule,
+    SharedModule,
     ReactiveFormsModule,
-    FlexLayoutModule,
-    MaterialModule,
   ],
   exports: [],
 })
--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { PastTrainingsComponent } from './past-trainings/past-trainings.component';
 import { CurrentTrainingComponent } from './current-training/current-training.component';
 import { NewTrainingComponent } from './new-training/new-training.component';
 import { StopTrainingComponent } from './current-training/stop-training.component';
-import { MaterialModule } from '../material.module';
-import { AuthModule } from '../auth/auth.module';
 import { TrainingComponent } from './training.component';
-import { FormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { FirestoreDatePipe } from './firestore-date.pipe';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -23,11 +19,7 @@ import { FirestoreDatePipe } from './firestore-date.pipe';
   ],
   imports: [
     AngularFirestoreModule,
-    CommonModule,
-    FlexLayoutModule,
-    FormsModule,
-    MaterialModule,
-    AuthModule,
+    SharedModule,
   ],
   entryComponents: [StopTrainingComponent]
 
