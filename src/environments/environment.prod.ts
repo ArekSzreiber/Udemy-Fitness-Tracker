a333cf35d714a84f219d7e822bb600ce86c0326e
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2021-01-21 01:05:21 +0100

    111. Wrap Up
    * add environment variables for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC89RKTNHiImeyx0QFesWXzIZgzLHCWRWc',
+    authDomain: 'fitness-tracker-46596.firebaseapp.com',
+    databaseURL: 'https://fitness-tracker-46596.firebaseio.com',
+    projectId: 'fitness-tracker-46596',
+    storageBucket: 'fitness-tracker-46596.appspot.com',
+    messagingSenderId: '598901745420',
+    appId: '1:598901745420:web:3099a6040f5c33b1f80b29',
+    measurementId: 'G-L5TPE6VSBH',
+  },
 };
